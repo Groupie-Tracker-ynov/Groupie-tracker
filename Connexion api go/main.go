39772ab114cb54d415f3de77fdabf49e4897ae31
KEIL_Enzo <enzo.keil06@icloud.com>
2023-02-06 11:44:57 +0100

    js

--- Connexion api go/main.go
+++ Connexion api go/main.go
@@ -2,16 +2,44 @@ package main
 
 import (
 	"fmt"
-
 	"html/template"
 	"net/http"
+	"sort"
 	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
+func motCroissant() []string {
+	words := []string{"a", "m", "d", "z", "b"}
+	sort.Strings(words)
+	fmt.Println(words)
+	return words
+}
+
+func motDecroissant() []string {
+	wordsInverse := []string{"a", "m", "d", "z", "b"}
+	sort.Sort(sort.Reverse(sort.StringSlice(wordsInverse)))
+	fmt.Println(wordsInverse)
+	return wordsInverse
+}
+
+func intCoissant() []int {
+	number := []int{1920, 2020, 2004, 1985, 2026}
+	sort.Ints(number)
+	fmt.Println(number)
+	return number
+}
+
+func intDecroissant() []int {
+	numberInverse := []int{1920, 2020, 2004, 1985, 2026}
+	sort.Sort(sort.Reverse(sort.IntSlice(numberInverse)))
+	fmt.Println(numberInverse)
+
+	return numberInverse
+}
+
 func main() {
-	fmt.Println("http://localhost:8000/")
 
 	router := gin.Default()
 	router.Static("/css", "./css")
@@ -23,8 +51,9 @@ func main() {
 	})
 
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{})
+		c.HTML(http.StatusOK, "index.html", gin.H{"motCroissant": motCroissant(), "motDecroissant": motDecroissant(), "intCoissant": intCoissant(), "intDecroissant": intDecroissant()})
 	})
 
 	router.Run("localhost:8000")
+
 }
